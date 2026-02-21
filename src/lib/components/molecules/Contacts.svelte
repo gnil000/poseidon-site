15e4739453f75455998c37305645141135c23af9
Elena Gnil <tony.vinz.one@gmail.com>
2026-02-21 18:51:57 +0300

    separate styles button default and contact buttons

--- src/lib/components/molecules/Contacts.svelte
+++ src/lib/components/molecules/Contacts.svelte
@@ -27,11 +27,29 @@
 {/snippet}
 
 <div class="flex flex-col items-center gap-5">
-	<h2 class="font-inter text-white text-xl md:text-2xl">Контакты</h2>
+	<h2 class="font-inter text-xl text-white md:text-2xl">Контакты</h2>
 	<div class="flex flex-col gap-3">
-		<Button href="tel:+79186505376" className="bg-sky-600">{@render buttonTelephone()}</Button>
-		<Button href="https://api.whatsapp.com/send?phone=79186505376" className="bg-sky-600">{@render buttonWhatsapp()}</Button>
-		<Button href="https://t.me/BorisPoseidon" className="bg-sky-600">{@render buttonTelegram()}</Button>
+		<Button
+			href="tel:+79186505376"
+			className="bg-sky-600 md:bg-transparent md:p-0
+			md:text-gray-200"
+			activeEffect={true}
+			animation="none">{@render buttonTelephone()}</Button
+		>
+		<Button
+			href="https://api.whatsapp.com/send?phone=79186505376"
+			className="bg-sky-600 md:bg-transparent md:p-0
+			md:text-gray-200"
+			activeEffect={true}
+			animation="none">{@render buttonWhatsapp()}</Button
+		>
+		<Button
+			href="https://t.me/BorisPoseidon"
+			className="bg-sky-600 md:bg-transparent md:p-0
+			md:text-gray-200"
+			activeEffect={true}
+			animation="none">{@render buttonTelegram()}</Button
+		>
 	</div>
 </div>
 
