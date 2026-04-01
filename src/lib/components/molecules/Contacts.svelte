544392568e14864ce891d183fa3bc0cf48be528d
Elena Gnil <tony.vinz.one@gmail.com>
2026-04-01 22:52:25 +0300

    added new desctop footer, start changing mobile footer

--- src/lib/components/molecules/Contacts.svelte
+++ src/lib/components/molecules/Contacts.svelte
@@ -1,72 +1,69 @@
 <script lang="ts">
-	import Button from '$lib/components/atoms/Button.svelte';
-	import Telephone from '$lib/components/atoms/icon/Telephone.svelte';
-	import Max from '$lib/components/atoms/icon/Max.svelte';
-	import Telegram from '$lib/components/atoms/icon/Telegram.svelte';
 </script>
 
-{#snippet buttonTelephone()}
-	<div class="flex flex-row items-center gap-2 px-4 py-2">
-		<Telephone></Telephone>
-		<span>Позвонить по телефону</span>
+<div
+	class="flex flex-row justify-evenly gap-30 text-slate-100"
+>
+	<div class="flex flex-col gap-y-5 pl-8 border-l border-sky-900/90">
+		<h3 class="text-xl font-semibold text-white">Наши контакты</h3>
+		<ul class="flex flex-col gap-y-2">
+			<li>
+				<a
+					href="tel:+79186505376"
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Звонок по телефону
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://web.max.ru/142125504"
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Написать в MAX
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://t.me/BorisPoseidon"
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Написать в Telegram
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://t.me/+lFciQGiQjtZjMzNi"
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Наш канал в Telegram
+				</a>
+			</li>
+		</ul>
 	</div>
-{/snippet}
 
-{#snippet buttonMax()}
-	<div class="flex flex-row items-center gap-2 px-4 py-2">
-		<Max fill="#ffffff"></Max>
-		<span>Написать в Max</span>
+	<div class="flex flex-col gap-y-5 pl-8 border-l border-sky-900/90">
+		<h3 class="text-xl font-semibold text-white">Наши партнёры</h3>
+		<ul class="flex flex-col gap-y-2">
+			<li>
+				<a
+					href=""
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Посейдон Сочи
+				</a>
+			</li>
+			<li>
+				<a
+					href=""
+					class="border-b border-white/30 text-slate-200 transition-colors duration-200 hover:text-white"
+				>
+					Наутилус Москва
+				</a>
+			</li>
+		</ul>
 	</div>
-{/snippet}
 
-{#snippet buttonTelegram()}
-	<div class="flex flex-row items-center gap-2 px-4 py-2">
-		<Telegram fill="#ffffff"></Telegram>
-		<span>Написать в Telegram</span>
-	</div>
-{/snippet}
-
-{#snippet buttonTelegramChannel()}
-	<div class="flex flex-row items-center gap-2 px-4 py-2">
-		<Telegram fill="#ffffff"></Telegram>
-		<span>Посетить наш Telegram канал</span>
-	</div>
-{/snippet}
-
-<div class="flex flex-col items-center gap-5">
-	<h2 class="font-inter text-xl text-white md:text-2xl">Контакты</h2>
-	<div class="flex flex-row items-center gap-5">
-		<div class="flex flex-col gap-3">
-			<Button
-				href="tel:+79186505376"
-				className="bg-sky-600 md:bg-transparent md:p-0
-			md:text-gray-200"
-				activeEffect={true}
-				animation="none">{@render buttonTelephone()}</Button
-			>
-			<Button
-				href="https://web.max.ru/142125504"
-				className="bg-sky-600 md:bg-transparent md:p-0
-			md:text-gray-200"
-				activeEffect={true}
-				animation="none">{@render buttonMax()}</Button
-			>
-			<Button
-				href="https://t.me/BorisPoseidon"
-				className="bg-sky-600 md:bg-transparent md:p-0
-			md:text-gray-200"
-				activeEffect={true}
-				animation="none">{@render buttonTelegram()}</Button
-			>
-			<Button
-				href="https://t.me/+lFciQGiQjtZjMzNi"
-				className="bg-sky-600 md:bg-transparent md:p-0
-			md:text-gray-200"
-				activeEffect={true}
-				animation="none">{@render buttonTelegramChannel()}</Button
-			>
-		</div>
-	</div>
 </div>
 
 <style>
--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+    import ContactsMobile from "$lib/components/molecules/ContactsMobile.svelte";
     import Contacts from "$lib/components/molecules/Contacts.svelte";
+
 	import Logo from "$lib/components/atoms/icon/Logo.svelte";
+
+    // let fadeOut = false;
+    let showIntro = false;
+	onMount(() => {
+		if (window.innerWidth < 768) {
+			showIntro = true;
+			setTimeout(() => {
+				// fadeOut = true;
+			}, 1500);
+		}
+	});
 </script>
 
 <footer class="w-full py-10 bg-obsidian-blue flex flex-col md:flex-row items-center justify-center gap-5">
     <div class="">
         <Logo width="100%" height="auto" fill="#ffffff"></Logo>
     </div>
+    {#if showIntro}
+    <section id="contacts">
+        <ContactsMobile></ContactsMobile>
+    </section>
+    {:else}
     <section id="contacts">
         <Contacts></Contacts>
     </section>
+    {/if}
 </footer>
 
 <style>
